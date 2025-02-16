3ea64e1a6c52f1bd5da7a2701cb6cd8d02396258
bax082024 <bax082024@gmail.com>
2025-02-16 16:49:41 +0100

    added scan network method

--- Program.cs
+++ Program.cs
@@ -33,4 +33,24 @@ class Program
 
     }
 
+    static void ScanNetwork(IPAddress startIp, IPAddress endIp)
+    {
+        long start = BitConverter.ToInt32(startIp.GetAddressBytes().Reverse().ToArray(), 0);
+        long end = BitConverter.ToInt32(endIp.GetAddressBytes().Reverse().ToArray(), 0);
+
+        for (long i = start; i <= end; i++)
+        {
+            var ipBytes = BitConverter.GetBytes(i).Reverse().ToArray();
+            var ip = new IPAddress(ipBytes);
+
+            if (PingHost(ip.ToString()))
+            {
+                Console.WriteLine($"[+] Active IP Found: {ip}");
+            }
+        }
+    }
+
+
+
+
 }
