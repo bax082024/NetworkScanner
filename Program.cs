30c5839584ad9e2f207395baf2d286487f158358
bax082024 <bax082024@gmail.com>
2025-02-16 16:57:08 +0100

    updated scannetwork method

--- Program.cs
+++ Program.cs
@@ -35,13 +35,17 @@ class Program
 
     static void ScanNetwork(IPAddress startIp, IPAddress endIp)
     {
-        long start = BitConverter.ToInt32(startIp.GetAddressBytes().Reverse().ToArray(), 0);
-        long end = BitConverter.ToInt32(endIp.GetAddressBytes().Reverse().ToArray(), 0);
+        byte[] startBytes = startIp.GetAddressBytes();
+        byte[] endBytes = endIp.GetAddressBytes();
 
-        for (long i = start; i <= end; i++)
+        // Convert IPs to integers
+        uint start = BitConverter.ToUInt32(startBytes.Reverse().ToArray(), 0);
+        uint end = BitConverter.ToUInt32(endBytes.Reverse().ToArray(), 0);
+
+        for (uint i = start; i <= end; i++)
         {
-            var ipBytes = BitConverter.GetBytes(i).Reverse().ToArray();
-            var ip = new IPAddress(ipBytes);
+            byte[] ipBytes = BitConverter.GetBytes(i).Reverse().ToArray();
+            IPAddress ip = new IPAddress(ipBytes);
 
             if (PingHost(ip.ToString()))
             {
@@ -49,6 +53,7 @@ class Program
             }
         }
     }
+
 
     static bool PingHost(string ip)
     {
